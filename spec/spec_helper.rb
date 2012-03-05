116507f107901d48d8ae4b53a5462cf1f10fa24f
Todor Grudev <tagrudev@gmail.com>
2012-03-05 14:07:15 +0200

    Adding rspec support + webmock helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+
+require 'rspec'
+require 'timecop'
+require 'memo_tomato'
+require 'helpers/webmock_helper'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    Timecop.return
+  end
+end
